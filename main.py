8d40d497350a3c6864fcebbf23c5a460689f49be
Denis Kondrashov <dkond@mail.ru>
2021-12-13 23:19:04 +0300

    cdr cmr tables imported

--- main.py
+++ main.py
@@ -4,21 +4,34 @@ import sqlite3
 from os import listdir
 from os.path import isfile, join
 
-table_name = 'table.db'
+cdr_table_name = 'cdr_table'
+cmr_table_name = 'cmr_table'
 CDR = 'CDR'
 
 def main():
     con = sqlite3.connect("my.db") # change to 'sqlite:///your_filename.db'
-    con = sqlite3.connect(":memory:")  # change to 'sqlite:///your_filename.db'
+    #con = sqlite3.connect(":memory:")  # change to 'sqlite:///your_filename.db'
     cur = con.cursor()
 
+    num_count: int = 1
     for fi in listdir(CDR):
         csv_file = join(CDR,fi)
         if isfile(csv_file):
             with open(csv_file, 'r') as csvfile:
+                print(num_count,'Importing data from', fi)
+                num_count += 1
                 df = pandas.read_csv(csvfile)
+                df.drop(0, axis=0, inplace=True)
                 print(df)
-                df.to_sql(table_name, con, if_exists='append', index=False)
+                if fi[0:3] == 'cdr':
+                    table_name = cdr_table_name
+                else:
+                    table_name = cmr_table_name
+                try:
+                    df.to_sql(table_name, con, if_exists='append', index=False)
+                except Exception as err:
+                    print(err)
+
 
 
 
