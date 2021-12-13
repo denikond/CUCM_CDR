381e45b7dc13cb5ff6d4e29035edd057c3b49d14
Denis Kondrashov <dkond@mail.ru>
2021-12-13 23:27:26 +0300

    cdr cmr tables imported

--- main.py
+++ main.py
@@ -33,24 +33,5 @@ def main():
                     print(err)
 
 
-
-
-'''
-    cur.execute("CREATE TABLE t (col1, col2);") # use your column names here
-
-    with open('data.csv','r') as fin: # `with` statement available in 2.5+
-        # csv.DictReader uses first line in file for column headings by default
-        dr = csv.DictReader(fin) # comma is default delimiter
-        to_db = [(i['col1'], i['col2']) for i in dr]
-
-    cur.executemany("INSERT INTO t (col1, col2) VALUES (?, ?);", to_db)
-    con.commit()
-    con.close()
-'''
-
-
-
-
-
 if __name__ == "__main__":
     main()
