8a609367687840dcc9475af3bbfdb690ee1a349c
Denis Kondrashov <dkond@mail.ru>
2021-12-19 01:55:24 +0300

    main connect to mssql

--- main.py
+++ main.py
@@ -3,22 +3,30 @@ import pandas
 import sqlite3
 from os import listdir
 from os.path import isfile, join
+import sqlalchemy
+import config
 
 cdr_table_name = 'cdr_table'
 cmr_table_name = 'cmr_table'
 CDR = 'CDR'
 
+
 def main():
-    con = sqlite3.connect("my.db") # change to 'sqlite:///your_filename.db'
-    #con = sqlite3.connect(":memory:")  # change to 'sqlite:///your_filename.db'
-    cur = con.cursor()
+    s_engine = 'mssql+pyodbc://' + config.Username + ':' \
+        + config.UPass + '@' + config.ServerName + '/' + config.Database \
+        + '?trusted_connection=no' + config.Driver
+    engine = sqlalchemy.create_engine(s_engine)
+    con = engine.connect()
+    # con = sqlite3.connect("my.db") # change to 'sqlite:///your_filename.db'
+    # con = sqlite3.connect(":memory:")  # change to 'sqlite:///your_filename.db'
+    #cur = con.cursor()
 
     num_count: int = 1
     for fi in listdir(CDR):
-        csv_file = join(CDR,fi)
+        csv_file = join(CDR, fi)
         if isfile(csv_file):
             with open(csv_file, 'r') as csvfile:
-                print(num_count,'Importing data from', fi)
+                print(num_count, 'Importing data from', fi)
                 num_count += 1
                 df = pandas.read_csv(csvfile)
                 df.drop(0, axis=0, inplace=True)
@@ -31,6 +39,7 @@ def main():
                     df.to_sql(table_name, con, if_exists='append', index=False)
                 except Exception as err:
                     print(err)
+
 
 
 if __name__ == "__main__":
