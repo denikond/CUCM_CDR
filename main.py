14f8f129a6a2c18d39a83b037aa87beeb235cc4f
denikond <74867259+denikond@users.noreply.github.com>
2022-03-29 23:14:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import csv
-import sqlite3
+#import csv
+import os
+#import sqlite3
 import pandas
 from os import listdir
 from os.path import isfile, join
 import sqlalchemy
 import config
+import time
 
-cdr_table_name = 'cdr.table'
+cdr_table_name = 'cdr_table'
 cmr_table_name = 'cmr_table'
 CDR = 'CDR'
 
@@ -21,25 +23,29 @@ def main():
     # con = sqlite3.connect(":memory:")  # change to 'sqlite:///your_filename.db'
     #cur = con.cursor()
 
-    num_count: int = 1
-    for fi in listdir(CDR):
-        csv_file = join(CDR, fi)
-        if isfile(csv_file):
-            with open(csv_file, 'r') as csvfile:
-                print(num_count, 'Importing data from', fi)
-                num_count += 1
-                df = pandas.read_csv(csvfile)
-                df.drop(0, axis=0, inplace=True)
-                print(df)
-                if fi[0:3] == 'cdr':
-                    table_name = cdr_table_name
-                else:
-                    table_name = cmr_table_name
-                try:
-                    df.to_sql(table_name, con, if_exists='append', index=False)
-                except Exception as err:
-                    print(err)
-                #aa = session.query(cmr_table_name).first()
+    while True:
+        num_count: int = 1
+        for fi in listdir(CDR):
+            csv_file = join(CDR, fi)
+            if isfile(csv_file):
+                with open(csv_file, 'r') as csvfile:
+                    print(num_count, 'Importing data from', fi)
+                    num_count += 1
+                    df = pandas.read_csv(csvfile)
+                    df.drop(0, axis=0, inplace=True)
+                    print(df)
+                    if fi[0:3] == 'cdr':
+                        table_name = cdr_table_name
+                    else:
+                        table_name = cmr_table_name
+                    try:
+                        df.to_sql(table_name, con, if_exists='append', index=False)
+                    except Exception as err:
+                        print(err)
+                    else:
+                        os.remove(csv_file)
+                    #aa = session.query(cmr_table_name).first()
+        time.sleep(60)
 
 
 if __name__ == "__main__":
